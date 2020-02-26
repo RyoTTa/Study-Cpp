f842edc417e6ca2bba3082e057ecfe62343a9541
RyoTTa <jhyn207@gmail.com>
2020-02-26 21:47:54 +0900

    이것이 C++이다 Chapter 1.1~1.7
    이것이 C++이다 Chapter 1.1~1.7

---
+++ 1.1/1.1/1.1.cpp
@@ -0,0 +1,65 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+void callByReference(int &rParam) {
+
+	rParam = 100;
+}
+int main(int argc, char* argv[])
+{
+	//출력
+    std::cout << "Hello World!" << std::endl; 
+
+	std::cout << 2 + 4 << std::endl;
+
+	//입력
+	std::cout << "Typing to anythings" << std::endl;
+	std::string temp;
+	//std::cin >> temp;
+	//std::cout << temp << std::endl;
+	
+	//자료형
+	int a = 99;
+	int b(a);
+	auto c(b);
+	std::cout << c << std::endl;
+
+	//동적할당
+	int *pInt = new int;
+	int *pIntA = new int[10];
+	*pInt = 10;
+	pIntA[0] = 11;
+	std::cout << *pInt <<' '<< pIntA[0] << std::endl;
+	delete pInt;
+	delete[] pIntA;
+
+	//참조자
+	int nInt = 12;
+	int &ref = nInt;
+	std::cout << ref << std::endl;
+	ref = 13;
+	std::cout << ref << std::endl;
+	callByReference(nInt);
+	std::cout << ref << std::endl;
+
+	//r-value(연산에 활용된 직후 소멸하는 r-value ??)
+	int &&rvdata = 2;
+	std::cout << rvdata << std::endl;
+
+	//Range-based FOR
+	int aList[5] = {10,20,30,40,50};
+
+	for (auto n : aList)
+		std::cout << n << ' ';
+	std::cout << std::endl;
+	for (auto &n : aList)
+		n = 10;
+	for (auto n : aList)
+		std::cout << n << ' ';
+	std::cout << std::endl;
+	
+
+	return 0;
+}
